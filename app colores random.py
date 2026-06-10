727cf5e217d10e065192add288c3473236cc891b
NicasioBayron <bayrongd932@gmail.com>
2026-06-09 23:40:41 -0400

    Cambio: Boton cambia de texto al apretarlo

--- app colores random.py
+++ app colores random.py
@@ -9,8 +9,10 @@ ventana.geometry("500x500")
 
 #Cambiar el color
 def cambiarColor():
+    bt = ["Cambio de boton 1", "Cambio de boton 2", "Cambio de boton 3", "Cambio de boton 4"]
     colores = ["red", "blue", "orange", "pink", "black", "white", "gray", "green", "yellow", "#FF5733"]
     ventana.config(bg=random.choice(colores))
+    boton.config(text=random.choice(bt))
 
 #Crear el boton
 boton = tk.Button(ventana, text="cambiar color", command=cambiarColor)
