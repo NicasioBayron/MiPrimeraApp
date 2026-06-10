92b4c509854f69aa9cdb7e20e9c3b402081a880f
NicasioBayron <bayrongd932@gmail.com>
2026-06-09 23:56:16 -0400

    Cambio version 3: Se quita el cambio de texto y se agrega numero de clics

--- app colores random.py
+++ app colores random.py
@@ -5,17 +5,16 @@ import random
 ventana = tk.Tk()
 ventana.title("App de prueba")
 ventana.geometry("500x500")
-
-
+contador = 0
 #Cambiar el color
 def cambiarColor():
-    bt = ["Cambio de boton 1", "Cambio de boton 2", "Cambio de boton 3", "Cambio de boton 4"]
+    global contador
+    contador = contador + 1
     colores = ["red", "blue", "orange", "pink", "black", "white", "gray", "green", "yellow", "#FF5733"]
     ventana.config(bg=random.choice(colores))
-    boton.config(text=random.choice(bt))
-
+    boton.config(text=f"Cambiar color, intento n°: {contador}")
 #Crear el boton
-boton = tk.Button(ventana, text="cambiar color", command=cambiarColor)
+boton = tk.Button(ventana, text=f"Cambiar color", command=cambiarColor)
 boton.pack(expand=True)
 
 ventana.mainloop()
